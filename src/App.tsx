570c66ca50bccc929577934db37dba3af6b77e4e
sulaymanhafizullah <sulayhafi33@gmail.com>
2025-12-01 11:30:54 -0600

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,24 @@ function App() {
 
     const [showAlert, setShowAlert] = useState(false);
 
+    const [game, setGame] = useState({
+        id: 1,
+        player: {
+            name: "John",
+        },
+    });
+
+    const [pizza, setPizza] = useState({
+        name: 'Spicy Pepperoni',
+        toppings: ['Mushrooms']
+    });
+
+    const handleClick = () => {
+        //setGame({...game, player: {...game.player, name: "Jane"}});
+        setPizza({...pizza, toppings: [...pizza.toppings, 'Pepperoni']});
+    }
+
+
     return (
         <>
             <Like onClick={() => {
